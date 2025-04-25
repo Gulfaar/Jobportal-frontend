9569c59c4dad79cad270d0c7a108efc5e4c641ed
Gulfaar <igulfaar@gmail.com>
2025-04-25 21:25:39 +0530

    image

--- app/Components/TaskSpecific/Fm.tsx
+++ app/Components/TaskSpecific/Fm.tsx
@@ -30,18 +30,19 @@ export const Fm: React.FC = () => {
 
       {/* Image Section */}
       <div className="w-full md:w-[37%] md:h-[55%] flex items-center">
-        <img
-          src="https://cdn.builder.io/api/v1/image/assets/928cdaf2e24e4e0f8ced4d57597d6970/c6c59f9f64f3d931e65c3207dc47edd4bcc02e16?placeholderIfAbsent=true"
-          alt="Career development illustration"
-          className="w-full h-[80%] object-cover rounded-3xl max-h-[400px] md:max-h-full"
-        />
-        
-      </div>
+  
+  
+  <img
+    src="/Asset-intgrity-2.png"
+    alt="Asset integrity illustration"
+    className="w-full h-[80%] object-cover rounded-3xl max-h-[400px] md:max-h-full"
+  />
+</div>
       
     </div>
     <Link href={"/FacilitationManagement"}>
     <button
-                className="flex gap-3 px-8 py-4 mt-[-70px] text-sm leading-tight text-center text-white uppercase bg-teal-800 hover:bg-teal-900 rounded-[50px] tracking-[3px] max-md:px-5 max-md:mt-10"
+                className="flex gap-3 px-8 py-4 mt-[-50px] text-sm leading-tight text-center text-white uppercase bg-teal-800 hover:bg-teal-900 rounded-[50px] tracking-[3px] max-md:px-5 max-md:mt-10"
                 aria-label="Get Started"
               >
                 <span className="grow my-auto">Learn more</span>
